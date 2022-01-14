e149cadfe575bf4d5222e67b4d2f33f0e42316b4
river <river@10.100.54.224>
2022-01-14 14:01:35 +0100

    Control check Jesús Antonio Rivero Gallardo

--- src/main/java/org/springframework/samples/petclinic/vacination/VaccinationService.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/VaccinationService.java
@@ -2,9 +2,17 @@ package org.springframework.samples.petclinic.vacination;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VaccinationService {
+	
+	@Autowired
+	private VaccinationRepository vr;
+	
     public List<Vaccination> getAll(){
-        return null;
+        return vr.findAll();
     }
 
     public List<Vaccine> getAllVaccines(){
@@ -12,11 +20,14 @@ public class VaccinationService {
     }
 
     public Vaccine getVaccine(String typeName) {
-        return null;
+        return vr.findVaccineByName(typeName);
     }
 
     public Vaccination save(Vaccination p) throws UnfeasibleVaccinationException {
-        return null;       
+    	if(p.getVaccinatedPet().getType()!=p.getVaccine().getPetType()) {
+    		throw new UnfeasibleVaccinationException();
+    	}   
+    	return p;
     }
 
     
